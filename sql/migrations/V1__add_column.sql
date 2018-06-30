6b34c3a4c9fd3bcc58103ddfb093c3f62ebacb8f
shizhiguo <shizhiguo@ibeesaas.com>
2018-06-30 17:11:45 +0800

    modify sql

--- sql/migrations/V1__add_column.sql
+++ sql/migrations/V1__add_column.sql
@@ -11,3 +11,6 @@ ADD COLUMN  actual_sending_address VARCHAR(50) NOT NULL DEFAULT '' COMMENT '实�
 ALTER TABLE `record_user_tx`
 MODIFY COLUMN `user_tx_status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态(0-初始状态,1-交易还未被打包,2-验证成功,21-验证失败（to不是平台地址）,22-验证失败（from不匹配）,23-验证失败（金额不匹配）,3-交易失败,4-交易不存在',
 MODIFY COLUMN `platform_tx_status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '打币交易状态(0-初始状态,1-打币中,2-成功,3-失败,4-交易作废)';
+
+ALTER TAble record_user_tx
+ADD COLUMN `distribution_batch_id` varchar(20) NOT NULL DEFAULT '' COMMENT '打币的批次id';
