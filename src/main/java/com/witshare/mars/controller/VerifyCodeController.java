8eb55b1fb0e9718f806d6e291fdfe3e8de4ef46c
zhangrongxin <zhangrongxin@ibeesaas.com>
2018-07-10 22:28:33 +0800

    add verify code api

--- src/main/java/com/witshare/mars/controller/VerifyCodeController.java
+++ src/main/java/com/witshare/mars/controller/VerifyCodeController.java
@@ -71,7 +71,7 @@ public class VerifyCodeController {
     @RequestMapping(value = "/img", method = RequestMethod.POST)
     public void checkVerifyCodeImg(HttpServletRequest request,
                                    HttpServletResponse response,
-                                   @RequestParam("code") String code,
+                                   @RequestParam("imgVerifyCode") String code,
                                    @RequestParam("imgToken") String token) throws Exception {
 
         captchaService.checkCaptcha(code, token);
